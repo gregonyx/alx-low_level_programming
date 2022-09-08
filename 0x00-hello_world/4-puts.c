73f0898b4ed841a6a239c943afd9ac116ab62b23
Greg Ejimofor <grgonyx@gmail.com>
2022-09-08 16:20:58 -0700

    Hello-word.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Printing My C Work
+ *
+ * Return: Always 0 (Successful, so far)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
